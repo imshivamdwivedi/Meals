67329383f4847e70caaf1fb3e9e85c39d3f882f2
imshivamdwivedi <dwivedishivam758@gmail.com>
2021-06-24 11:36:23 +0530

    Meals app

---
+++ lib/category_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import './category_meals_screen.dart';
+
+class CatgoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  CatgoryItem({required this.id, required this.title, required this.color});
+
+  void _selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .pushNamed('/categorie-meals', arguments: {'id': id, 'title': title});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Center(child: Text(title)),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color.withOpacity(0.5), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15)),
+      ),
+    );
+  }
+}
